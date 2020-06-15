19795ab1597bb3cc349b12306c67cdd1e7121271
Coty Getzelman <coty@wanderingcorgi.com>
2020-06-14 23:29:18 -0700

    wip surface placement

--- Assets/Scripts/Spline.cs
+++ Assets/Scripts/Spline.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using System;
+using UnityEditorInternal;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -14,53 +15,51 @@ public class SplineEditor : Editor
     public int SelectedPoint = -1;
     public bool MirrorAnchors = true;
 
+    private bool PlacingPoint;
+    public SplinePlacePointMode PlaceMode;
+    public LayerMask PlaceLayerMask;
+    public Vector3 PlacePlaneOffset;
+    public Vector3 PlacePlaneNormal;
+    
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
 
         var instance = (Spline) target;
 
-        if(GUILayout.Button("Add Point"))
+        if(GUILayout.Button("Append Point"))
         {
-            Undo.RegisterCompleteObjectUndo(instance, "Add Point");
-
-
-            if(instance.Mode == SplineMode.Linear)
-            {
-                var newArray = new SplinePoint[instance.Points.Length + 1];
-                for(var i = 0; i < instance.Points.Length; ++i)
-                {
-                    newArray[i] = instance.Points[i];
-                }
-
-                instance.Points = newArray;
-
-                instance.Points[instance.Points.Length - 1] = new SplinePoint(instance.transform.position + Vector3.up, Vector3.up);
-            }
-            else if(instance.Mode == SplineMode.Bezier)
-            {
-
-                var newArray = new SplinePoint[instance.Points.Length + 3];
-                for (var i = 0; i < instance.Points.Length; ++i)
-                {
-                    newArray[i] = instance.Points[i];
-                }
-
-                instance.Points = newArray;
-
-                var last0 = instance.Points[instance.Points.Length - 5];
-                var last1 = instance.Points[instance.Points.Length - 4];
-                var lastDirection = (last1.position - last0.position).normalized;
-
-                instance.Points[instance.Points.Length - 3] = new SplinePoint(last1.position + lastDirection * 1, new Vector3(0, 1, 0));
-                instance.Points[instance.Points.Length - 2] = new SplinePoint(last1.position + lastDirection * 2, new Vector3(0, 1, 0));
-                instance.Points[instance.Points.Length - 1] = new SplinePoint(last1.position + lastDirection * 3, new Vector3(0, 1, 0));
-            }
-
-            EditorUtility.SetDirty(instance);
+            var lastPos = instance.GetPoint(1f);
+            AppendPoint(instance, lastPos.position, lastPos.up);
         }
 
 
+        if(!PlacingPoint && GUILayout.Button("Start Placing Points"))
+        {
+            PlacingPoint = !PlacingPoint;
+        }
+        else if (PlacingPoint && GUILayout.Button("Stop Placing Points"))
+        {
+            PlacingPoint = !PlacingPoint;
+        }
+
+        if(PlacingPoint)
+        {
+            PlaceMode = (SplinePlacePointMode) EditorGUILayout.EnumPopup("Place Point Mode", PlaceMode);
+
+            if(PlaceMode == SplinePlacePointMode.Surface)
+            {
+                LayerMask tempMask = EditorGUILayout.MaskField("Surface Layers", InternalEditorUtility.LayerMaskToConcatenatedLayersMask(PlaceLayerMask), InternalEditorUtility.layers);
+                PlaceLayerMask = InternalEditorUtility.ConcatenatedLayersMaskToLayerMask(tempMask);
+            }
+
+            if(PlaceMode == SplinePlacePointMode.Plane)
+            {
+                PlacePlaneOffset = EditorGUILayout.Vector3Field("Plane Offset", PlacePlaneOffset);
+                PlacePlaneNormal = EditorGUILayout.Vector3Field("Plane Normal", PlacePlaneNormal);
+            }
+        }
+        
         var optionsStr = new string[instance.Points.Length + 1];
         var optionsInt = new int [instance.Points.Length + 1];
 
@@ -93,84 +92,207 @@ public class SplineEditor : Editor
 
     }
 
+
+    private void AppendPoint(Spline instance, Vector3 position, Vector3 up)
+    {
+        Undo.RegisterCompleteObjectUndo(instance, "Append Point");
+        
+        if (instance.Mode == SplineMode.Linear)
+        {
+            var newArray = new SplinePoint[instance.Points.Length + 1];
+            for (var i = 0; i < instance.Points.Length; ++i)
+            {
+                newArray[i] = instance.Points[i];
+            }
+
+            instance.Points = newArray;
+
+            instance.Points[instance.Points.Length - 1] = new SplinePoint(position, up);
+        }
+        else if (instance.Mode == SplineMode.Bezier)
+        {
+
+            var newArray = new SplinePoint[instance.Points.Length + 3];
+            for (var i = 0; i < instance.Points.Length; ++i)
+            {
+                newArray[i] = instance.Points[i];
+            }
+
+            instance.Points = newArray;
+
+            var last0 = instance.Points[instance.Points.Length - 5];
+            var last1 = instance.Points[instance.Points.Length - 4];
+            var lastDirection = (last1.position - last0.position).normalized;
+
+            instance.Points[instance.Points.Length - 3] = new SplinePoint(position - lastDirection * 2, up);
+            instance.Points[instance.Points.Length - 2] = new SplinePoint(position - lastDirection * 1, up);
+            instance.Points[instance.Points.Length - 1] = new SplinePoint(position - lastDirection * 0, up);
+        }
+
+        EditorUtility.SetDirty(instance);
+    }
+
     private void OnSceneGUI()
     {
         var instance = (Spline)target;
-        if(SelectedPoint >= 0)
+
+        if(PlacingPoint)
         {
+            DrawPlacePointView(instance); 
+        }
+        else if(SelectedPoint >= 0)
+        {
+            DrawSelectedSplineHandle(instance); 
+        }
+        
+        DrawSelectablePoints(instance);
+    }
 
-            var splinePoint = instance.Points[SelectedPoint];
+    private void DrawPlacePointView(Spline instance)
+    {
+        Handles.color = Color.red;
 
-
-            Handles.color = Color.white;
-            var anyMoved = DrawHandle(Vector3.zero, ref splinePoint.position, out Vector3 splinePointDelta);
-
-            var pointIsHandle = SelectedPoint % 3 != 0;
-            if(pointIsHandle)
+        if(PlaceMode == SplinePlacePointMode.Plane)
+        {
+            DrawHandle(Vector3.zero, ref PlacePlaneOffset, out Vector3 offsetDelta);
+        }
+        
+        var mousePosition = Event.current.mousePosition;
+        var worldRay = HandleUtility.GUIPointToWorldRay(mousePosition);
+        worldRay.origin += worldRay.direction * Camera.current.nearClipPlane; 
+        
+        var selected = Handles.Button(worldRay.origin, Quaternion.identity, 0.05f, 0.05f, Handles.DotHandleCap);
+        if (selected)
+        {
+            switch(PlaceMode)
             {
-                var pointIndex0 = SelectedPoint - SelectedPoint % 3 + 0;
-                var pointIndex1 = SelectedPoint - SelectedPoint % 3 + 3;
+                case SplinePlacePointMode.CameraPlane:
+                    AppendPoint(instance, worldRay.origin, Vector3.up);
+                    break;
+                case SplinePlacePointMode.Plane:
 
-                var index = SelectedPoint % 3 == 1 ? pointIndex0 : pointIndex1;
-
-                var anchorPoint = instance.Points[index];
-
-                Handles.color = Color.gray;
-                Handles.DrawLine( splinePoint.position, anchorPoint.position );
-
-
-                if(MirrorAnchors && SelectedPoint != 1 && SelectedPoint != instance.Points.Length - 2)
-                {
-                    var otherHandleIndex = index == pointIndex0 
-                        ? pointIndex0 - 1 
-                        : pointIndex1 + 1;
-
-                    var otherHandlePoint = instance.Points[otherHandleIndex];
-
-                    if(anyMoved)
+                    if(PlacePlaneNormal.sqrMagnitude < 0.01f)
                     {
-                        var toAnchorPoint = anchorPoint.position - splinePoint.position;
-                        var otherHandlePosition = anchorPoint.position + toAnchorPoint;
-                        otherHandlePoint.position = otherHandlePosition;
-                        instance.Points[otherHandleIndex] = otherHandlePoint;
+                        PlacePlaneNormal = Vector3.up;
                     }
 
-                    Handles.DrawLine(otherHandlePoint.position, anchorPoint.position);
-                }
+                    PlacePlaneNormal = PlacePlaneNormal.normalized;
+                    
+                    var projectedOnPlane = Vector3.ProjectOnPlane(worldRay.origin - PlacePlaneOffset, PlacePlaneNormal) + PlacePlaneOffset;
+                    AppendPoint(instance, projectedOnPlane, PlacePlaneNormal);
+
+                    break;
+                case SplinePlacePointMode.Surface:
+
+
+                    RaycastHit info = new RaycastHit();
+                    bool hit = false;
+
+                    var overObjects = HandleUtility.PickRectObjects(new Rect(mousePosition, Vector2.one * 0.25f));
+                    foreach(var go in overObjects)
+                    {
+                        if (PlaceLayerMask == (PlaceLayerMask | (1 << go.layer)))
+                        {
+                            hit = RXLookingGlass.IntersectRayGameObject(worldRay, go, out info);
+                            if(hit)
+                            {
+                                break; 
+                            }
+                        }
+                    }
+
+                    if(hit)
+                    {
+                        AppendPoint(instance, info.point, info.normal);
+                    }
+                    else
+                    {
+                        Debug.LogError("No hit.");
+                    }
+
+
+                    break; 
             }
-            else
+        }
+    }
+
+    private void DrawSelectedSplineHandle(Spline instance)
+    {
+
+        var splinePoint = instance.Points[SelectedPoint];
+
+
+        Handles.color = Color.white;
+        var anyMoved = DrawHandle(Vector3.zero, ref splinePoint.position, out Vector3 splinePointDelta);
+
+        var pointIsHandle = SelectedPoint % 3 != 0;
+        if (pointIsHandle)
+        {
+            var pointIndex0 = SelectedPoint - SelectedPoint % 3 + 0;
+            var pointIndex1 = SelectedPoint - SelectedPoint % 3 + 3;
+
+            var index = SelectedPoint % 3 == 1 ? pointIndex0 : pointIndex1;
+
+            var anchorPoint = instance.Points[index];
+
+            Handles.color = Color.gray;
+            Handles.DrawLine(splinePoint.position, anchorPoint.position);
+
+
+            if (MirrorAnchors && SelectedPoint != 1 && SelectedPoint != instance.Points.Length - 2)
             {
-                if(instance.Points.Length > 1)
+                var otherHandleIndex = index == pointIndex0
+                    ? pointIndex0 - 1
+                    : pointIndex1 + 1;
+
+                var otherHandlePoint = instance.Points[otherHandleIndex];
+
+                if (anyMoved)
                 {
-                    var handleIndex0 = SelectedPoint != 0 ? SelectedPoint - 1 : SelectedPoint + 1;
-                    var handleIndex1 = SelectedPoint != instance.Points.Length - 1 ? SelectedPoint + 1 : SelectedPoint - 1;
-
-                    var handle0 = instance.Points[handleIndex0];
-                    var handle1 = instance.Points[handleIndex1];
-
-                    handle0.position = handle0.position + splinePointDelta;
-                    handle1.position = handle1.position + splinePointDelta;
-
-                    instance.Points[handleIndex0] = handle0;
-                    instance.Points[handleIndex1] = handle1;
-
-                    Handles.color = Color.gray;
-                    Handles.DrawLine(splinePoint.position, handle0.position);
-                    Handles.DrawLine(splinePoint.position, handle1.position);
+                    var toAnchorPoint = anchorPoint.position - splinePoint.position;
+                    var otherHandlePosition = anchorPoint.position + toAnchorPoint;
+                    otherHandlePoint.position = otherHandlePosition;
+                    instance.Points[otherHandleIndex] = otherHandlePoint;
                 }
+
+                Handles.DrawLine(otherHandlePoint.position, anchorPoint.position);
             }
-            
-            if(anyMoved)
+        }
+        else
+        {
+            if (instance.Points.Length > 1)
             {
-                Undo.RegisterCompleteObjectUndo(instance, "Move Point");
-                instance.Points[SelectedPoint] = splinePoint;
+                var handleIndex0 = SelectedPoint != 0 ? SelectedPoint - 1 : SelectedPoint + 1;
+                var handleIndex1 = SelectedPoint != instance.Points.Length - 1 ? SelectedPoint + 1 : SelectedPoint - 1;
+
+                var handle0 = instance.Points[handleIndex0];
+                var handle1 = instance.Points[handleIndex1];
+
+                handle0.position = handle0.position + splinePointDelta;
+                handle1.position = handle1.position + splinePointDelta;
+
+                instance.Points[handleIndex0] = handle0;
+                instance.Points[handleIndex1] = handle1;
+
+                Handles.color = Color.gray;
+                Handles.DrawLine(splinePoint.position, handle0.position);
+                Handles.DrawLine(splinePoint.position, handle1.position);
             }
         }
 
-        // selectable points
-        for(var p = 0; p < instance.Points.Length; ++p)
+        if (anyMoved)
         {
-            if(p == SelectedPoint)
+            Undo.RegisterCompleteObjectUndo(instance, "Move Point");
+            instance.Points[SelectedPoint] = splinePoint;
+        }
+    }
+
+    private void DrawSelectablePoints(Spline instance)
+    {
+
+        for (var p = 0; p < instance.Points.Length; ++p)
+        {
+            if (p == SelectedPoint)
             {
                 continue;
             }
@@ -181,10 +303,10 @@ public class SplineEditor : Editor
 
 
 
-            if(isHandle)
+            if (isHandle)
             {
                 // when nothing is selected, do not draw handles 
-                if(SelectedPoint == -1)
+                if (SelectedPoint == -1)
                 {
                     continue;
                 }
@@ -194,10 +316,10 @@ public class SplineEditor : Editor
                 if (isSelectedHandle)
                 {
                     var handleIndex = p % 3;
-                    if(handleIndex == 1)
+                    if (handleIndex == 1)
                     {
                         var parentPoint = p - 2;
-                        if(parentPoint != SelectedPoint)
+                        if (parentPoint != SelectedPoint)
                         {
                             continue;
                         }
@@ -205,9 +327,9 @@ public class SplineEditor : Editor
                     else
                     {
                         var parentPoint = p + 2;
-                        if(parentPoint != SelectedPoint)
+                        if (parentPoint != SelectedPoint)
                         {
-                            continue; 
+                            continue;
                         }
                     }
                 }
@@ -225,14 +347,14 @@ public class SplineEditor : Editor
             Handles.color = isHandle ? Color.green : Color.blue;
 
             var selected = Handles.Button(position, Quaternion.identity, 0.25f, 0.25f, Handles.DotHandleCap);
-            if(selected)
+            if (selected)
             {
                 Undo.RegisterCompleteObjectUndo(this, "Selected Point");
                 SelectedPoint = p;
             }
         }
     }
-
+    
     private bool DrawHandle(Vector3 offset, ref Vector3 positionRef, out Vector3 delta)
     {
         var startPosition = positionRef;
@@ -263,6 +385,13 @@ public enum SplineMode
 {
     Linear,
     Bezier,
+}
+
+public enum SplinePlacePointMode
+{
+    CameraPlane,
+    Plane,
+    Surface,
 }
 
 public class Spline : MonoBehaviour
