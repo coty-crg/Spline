dbab470fe1db1892b7af4ce456cc4c45cab60a5e
Coty Getzelman <coty-crg@live.com>
2020-06-25 21:21:18 -0700

    moving some stuff and added follow spline script

---
+++ Assets/SplineTool/Scripts/Examples/TransformFollowSpline.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformFollowSpline : MonoBehaviour
+{
+    public Spline FollowSpline;
+    public float FollowSpeed = 1f;
+    public bool FollowRotation;
+
+    private void Update()
+    {
+        if(FollowSpline == null)
+        {
+            return;
+        }
+
+        var t = FollowSpline.ProjectOnSpline_t(transform.position);
+
+        var projectedForward = FollowSpline.GetForward(t);
+        var projectedPoint = FollowSpline.GetPoint(t);
+        projectedPoint.position += projectedForward * (FollowSpeed * Time.deltaTime);
+
+        transform.position = projectedPoint.position;
+
+        if(FollowRotation)
+        {
+            transform.rotation = projectedPoint.rotation;
+        }
+    }
+
+}
