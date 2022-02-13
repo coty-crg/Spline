fa300800a7dcb9f1c6a659890e0f70a96c2a8fb6
Coty Getzelman <coty-crg@live.com>
2022-02-13 09:45:54 -0800

    added various tests to the repeatable mesh editor

--- Assets/SplineTool/Scripts/ExampleUsages/ProceduralMeshing/Editor/SplineMeshBuilder_RepeatingMesh_Editor.cs
+++ Assets/SplineTool/Scripts/ExampleUsages/ProceduralMeshing/Editor/SplineMeshBuilder_RepeatingMesh_Editor.cs
@@ -11,6 +11,13 @@ namespace CorgiSpline
     {
         protected SerializedProperty RepeatableMesh;
         protected SerializedProperty UseRepeatingMeshUVs;
+        private List<Vector3> _vertexCache = new List<Vector3>();
+        private List<int> _vertexMinZCache = new List<int>();
+        private List<int> _vertexMaxZCache = new List<int>();
+
+        private Mesh _prevMeshVerified;
+        private bool _passed_MinMaxMatch;
+        private bool _passed_XYMatch;
 
         protected override void OnEnable()
         {
@@ -60,6 +67,9 @@ namespace CorgiSpline
                                 var has_uv0 = instance.RepeatableMesh.HasVertexAttribute(UnityEngine.Rendering.VertexAttribute.TexCoord0);
                                 var has_color = instance.RepeatableMesh.HasVertexAttribute(UnityEngine.Rendering.VertexAttribute.Color);
 
+                                // run various tests on the mesh 
+                                VerifyMeshTests(instance.RepeatableMesh);
+
                                 if (!has_normals)
                                 {
                                     EditorGUILayout.HelpBox("RepeatableMesh does not contain normals. It is recommended to add them.",
@@ -83,6 +93,17 @@ namespace CorgiSpline
                                     EditorGUILayout.HelpBox("RepeatableMesh does not contain vertex colors. It is recommended to add them.",
                                         MessageType.Info);
                                 }
+
+                                if (!_passed_MinMaxMatch)
+                                {
+                                    EditorGUILayout.HelpBox($"The number of vertices at the z start and the number of vertices at z end do NOT match! " +
+                                        $"start: {_vertexMinZCache.Count}, end: {_vertexMaxZCache.Count}", MessageType.Error);
+                                }
+
+                                if (!_passed_XYMatch)
+                                {
+                                    EditorGUILayout.HelpBox($"The vertices at the start of the mesh do not match the vertices at the end of the mesh! ", MessageType.Warning);
+                                }
                             }
                             EditorGUILayout.EndVertical();
                         }
@@ -104,6 +125,83 @@ namespace CorgiSpline
             if (GUI.changed)
             {
                 serializedObject.ApplyModifiedProperties();
+            }
+        }
+
+        private void VerifyMeshTests(Mesh mesh)
+        {
+            // repetable mesh vertices match check 
+            if (_prevMeshVerified == null || _prevMeshVerified != mesh)
+            {
+                _prevMeshVerified = mesh;
+
+                _vertexCache.Clear();
+                mesh.GetVertices(_vertexCache);
+
+                var minZ = float.MaxValue;
+                foreach (var vert in _vertexCache)
+                {
+                    if (vert.z < minZ)
+                    {
+                        minZ = vert.z;
+                    }
+                }
+
+                var maxZ = float.MinValue;
+                foreach (var vert in _vertexCache)
+                {
+                    if (vert.z > maxZ)
+                    {
+                        maxZ = vert.z;
+                    }
+                }
+
+                _vertexMinZCache.Clear();
+                for (var i = 0; i < _vertexCache.Count; ++i)
+                {
+                    var vert = _vertexCache[i];
+                    if (Mathf.Abs(vert.z - minZ) < 0.0001f)
+                    {
+                        _vertexMinZCache.Add(i);
+                    }
+                }
+
+                _vertexMaxZCache.Clear();
+                for (var i = 0; i < _vertexCache.Count; ++i)
+                {
+                    var vert = _vertexCache[i];
+                    if (Mathf.Abs(vert.z - maxZ) < 0.0001f)
+                    {
+                        _vertexMaxZCache.Add(i);
+                    }
+                }
+
+                _passed_MinMaxMatch = _vertexMinZCache.Count == _vertexMaxZCache.Count;
+                _passed_XYMatch = true; 
+
+                if (_passed_MinMaxMatch)
+                {
+                    // sort vertice indices by angle around the plane of their start and end caps 
+                    // note: this assumes the z values match perfectly 
+                    _vertexMinZCache.Sort((ai, bi) => Mathf.Atan2(_vertexCache[ai].y, _vertexCache[ai].x).CompareTo(Mathf.Atan2(_vertexCache[bi].y, _vertexCache[bi].x)));
+                    _vertexMaxZCache.Sort((ai, bi) => Mathf.Atan2(_vertexCache[ai].y, _vertexCache[ai].x).CompareTo(Mathf.Atan2(_vertexCache[bi].y, _vertexCache[bi].x)));
+                    
+                    for (var i = 0; i < _vertexMinZCache.Count; ++i)
+                    {
+                        var min_z_i = _vertexMinZCache[i];
+                        var max_z_i = _vertexMaxZCache[i];
+
+
+                        var vert_min_z = _vertexCache[min_z_i];
+                        var vert_max_z = _vertexCache[max_z_i];
+
+                        if(Mathf.Abs(vert_min_z.x - vert_max_z.x) > 0.0001f || Mathf.Abs(vert_min_z.y - vert_max_z.y) > 0.0001f)
+                        {
+                            _passed_XYMatch = false; 
+                            break;
+                        }
+                    }
+                }
             }
         }
 
