6b44c68d4ee53129be95592c4af8bba0ed7277a7
Coty Getzelman <coty-crg@live.com>
2022-02-13 16:44:51 -0800

    added a spline prefab repeater script

---
+++ Assets/SplineTool/Scripts/ExampleUsages/Editors/PrefabRepeaterEditor.cs
@@ -0,0 +1,119 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace CorgiSpline
+{ 
+    [CustomEditor(typeof(PrefabRepeater))]
+    public class PrefabRepeaterEditor : Editor
+    {
+        protected SerializedProperty SplineReference;
+        protected SerializedProperty PrefabToRepeat;
+        protected SerializedProperty RepeatCount;
+        protected SerializedProperty RepeatPercentage;
+        protected SerializedProperty RefreshOnEnable;
+        protected SerializedProperty RefreshOnUpdate;
+        protected SerializedProperty PositionOffset;
+        protected SerializedProperty ScaleOffset;
+        protected SerializedProperty RotationEulorOffset;
+
+        private void OnEnable()
+        {
+            SplineReference     = serializedObject.FindProperty("SplineReference");
+            PrefabToRepeat      = serializedObject.FindProperty("PrefabToRepeat");
+            RepeatCount         = serializedObject.FindProperty("RepeatCount");
+            RepeatPercentage    = serializedObject.FindProperty("RepeatPercentage");
+            RefreshOnEnable     = serializedObject.FindProperty("RefreshOnEnable");
+            RefreshOnUpdate     = serializedObject.FindProperty("RefreshOnUpdate");
+            PositionOffset      = serializedObject.FindProperty("PositionOffset");
+            ScaleOffset         = serializedObject.FindProperty("ScaleOffset");
+            RotationEulorOffset = serializedObject.FindProperty("RotationEulorOffset");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            var instance = (PrefabRepeater) target;
+
+            GUILayout.BeginVertical("GroupBox");
+            {
+                EditorGUILayout.LabelField("Spline", EditorStyles.boldLabel);
+                EditorGUILayout.PropertyField(SplineReference);
+
+                if (instance.SplineReference == null)
+                {
+                    EditorGUILayout.HelpBox("SplineReference is null. Please assign a spline to sample from!", MessageType.Error);
+                }
+            }
+            GUILayout.EndVertical();
+
+            GUILayout.BeginVertical("GroupBox");
+            {
+                EditorGUILayout.LabelField("Prefab", EditorStyles.boldLabel);
+                EditorGUILayout.PropertyField(PrefabToRepeat);
+                EditorGUILayout.Space();
+
+                if (instance.PrefabToRepeat == null)
+                {
+                    EditorGUILayout.HelpBox("PrefabToRepeat is null. Please assign a GameObject to repeat!", MessageType.Error);
+                }
+                else
+                {
+                    // draw preview 
+                    var assetPreview = AssetPreview.GetAssetPreview(instance.PrefabToRepeat);
+                    if (assetPreview != null)
+                    {
+                        EditorGUILayout.Space();
+                        var style = new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter };
+                        GUILayout.Label(assetPreview, style);
+                        EditorGUILayout.Space();
+                    }
+                }
+            }
+            GUILayout.EndVertical();
+
+            GUILayout.BeginVertical("GroupBox");
+            {
+                EditorGUILayout.LabelField("Settings", EditorStyles.boldLabel);
+                EditorGUILayout.PropertyField(RepeatCount);
+                EditorGUILayout.PropertyField(RepeatPercentage);
+
+                EditorGUILayout.PropertyField(PositionOffset);
+                EditorGUILayout.PropertyField(ScaleOffset);
+                EditorGUILayout.PropertyField(RotationEulorOffset);
+
+                EditorGUILayout.PropertyField(RefreshOnEnable);
+                EditorGUILayout.PropertyField(RefreshOnUpdate);
+            }
+            GUILayout.EndVertical();
+
+            if (GUI.changed)
+            {
+                serializedObject.ApplyModifiedProperties();
+            }
+
+            // draw default inspector.. 
+            // base.OnInspectorGUI();
+        }
+
+        [MenuItem("GameObject/CorgiSpline/Spline Prefab Repeater", priority = 10)]
+        public static void MenuItemCreateMeshBuilder_Cubey()
+        {
+            var editorConfig = SplineEditorConfig.FindConfig();
+            var newGameobject = new GameObject("NewSplinePrefabRepeater");
+
+            var spline = newGameobject.AddComponent<Spline>();
+                spline.SetSplineSpace(Space.Self, false);
+
+            var meshBuilder = newGameobject.AddComponent<PrefabRepeater>();
+                meshBuilder.SplineReference = spline;
+
+            if (Selection.activeTransform != null)
+            {
+                newGameobject.transform.SetParent(Selection.activeTransform);
+            }
+        }
+    }
+}
+#endif
---
+++ Assets/SplineTool/Scripts/ExampleUsages/PrefabRepeater.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CorgiSpline
+{
+    public class PrefabRepeater : MonoBehaviour
+    {
+        [Tooltip("Spline to use for prefab repeating.")] 
+        public Spline SplineReference; 
+
+        [Tooltip("Prefab to repeat along the spline.")] 
+        public GameObject PrefabToRepeat;
+
+        [Tooltip("Number of times to repeat this prefab along the spline.")] [Range(0, 256)] 
+        public int RepeatCount = 16;
+
+        [Tooltip("For animating this effect.")] [Range(0f, 1f)] 
+        public float RepeatPercentage = 1f;
+
+        [Tooltip("OnEnable() will spawn prefabs along the spline.")] 
+        public bool RefreshOnEnable = true;
+
+        [Tooltip("OnUpdate() will spawn prefabs along the spline, destroying any existing ones.")] 
+        public bool RefreshOnUpdate;
+
+        [Tooltip("Position offset for the prefab from the spline.")] 
+        public Vector3 PositionOffset;
+
+        [Tooltip("Scale offset for the prefab from the spline.")]
+        public Vector3 ScaleOffset = new Vector3(1, 1, 1);
+
+        [Tooltip("Rotation offset for the prefab from the spline.")] 
+        public Vector3 RotationEulorOffset;
+
+        private void OnEnable()
+        {
+            if(RefreshOnEnable)
+            {
+                Refresh();   
+            }
+        }
+
+        private void Update()
+        {
+            if (RefreshOnUpdate)
+            {
+                Refresh();
+            }
+        }
+
+        public void Refresh()
+        {
+            if (SplineReference == null || PrefabToRepeat == null)
+            {
+                return; 
+            }
+
+            ClearPrefabs();
+
+            var parent = transform;
+
+            for(var s = 0; s < RepeatCount; ++s)
+            {
+                var t = (float) s / RepeatCount;
+                if (t > RepeatPercentage) break;
+
+                var splinePoint = SplineReference.GetPoint(t);
+
+                var forward = splinePoint.rotation * Vector3.forward;
+                var right = splinePoint.rotation * Vector3.right;
+                var up = splinePoint.rotation * Vector3.up;
+                var offset = PositionOffset.x * right + PositionOffset.y * up + PositionOffset.z * forward;
+
+                var go = GameObject.Instantiate(PrefabToRepeat, parent);
+                var transform = go.transform;
+                    transform.SetPositionAndRotation(splinePoint.position + offset, splinePoint.rotation * Quaternion.Euler(RotationEulorOffset));
+                    transform.localScale = Vector3.Scale(splinePoint.scale, ScaleOffset);
+            }
+        }
+
+        public void ClearPrefabs()
+        {
+            var parent = transform;
+            var childCount = parent.childCount;
+            
+            var toDestroy = new List<GameObject>();
+
+            for(var t = 0; t < childCount; ++t)
+            {
+                toDestroy.Add(parent.GetChild(t).gameObject); 
+            }
+
+            for (var t = 0; t < childCount; ++t)
+            {
+                var go = toDestroy[t];
+                GameObject.Destroy(go); 
+            }
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (SplineReference == null) return;
+
+            Gizmos.color = new Color(1, 1, 1, 0.75f);
+
+            for (var s = 0; s < RepeatCount; ++s)
+            {
+                var t = (float)s / RepeatCount;
+                if (t > RepeatPercentage) break;
+
+                var splinePoint = SplineReference.GetPoint(t);
+
+                var forward = splinePoint.rotation * Vector3.forward;
+                var right = splinePoint.rotation * Vector3.right;
+                var up = splinePoint.rotation * Vector3.up;
+                var offset = PositionOffset.x * right + PositionOffset.y * up + PositionOffset.z * forward;
+
+                Gizmos.DrawSphere(splinePoint.position + offset, splinePoint.scale.magnitude * ScaleOffset.magnitude); 
+            }
+        }
+    }
+}
